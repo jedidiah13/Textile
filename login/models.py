b9139908441e81e97cc334e1b8e6af26ae9fda07
pdf2e <pdf2e@mtmail.mtsu.edu>
2014-04-11 14:53:51 -0500

    added User database stuffzzzzz
    added User database stuffzzzzz

--- login/models.py
+++ login/models.py
@@ -5,7 +5,13 @@ from loginRouter import *
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
     confirmation_code = models.CharField(max_length=128)
-    def __unicode__(self):
+    reset_code = models.CharField(max_length=128)
+    address_lineOne = models.CharField(max_length=128)
+    address_lineTwo = models.CharField(max_length=128)
+    city = models.CharField(max_length=128)
+    State = models.CharField(max_length=128)
+    zipCode = models.CharField(max_length=10)
+    def __unicode__(self): 
 		return self.user.username    
     
 

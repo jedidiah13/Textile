0198a955d684712828fa9f74a7948ba55f7d1b21
devgr <devgr4@gmail.com>
2014-04-14 13:01:47 -0500

    Revert "Added orders to the user db"
    This reverts commit 4dd7d60fcde6f35f96ace77a937ee967f0dec5db.

--- login/admin.py
+++ login/admin.py
@@ -1,9 +1,3 @@
 from django.contrib import admin
-from login.models import Order, OrderItem
-
-class OrderAdmin(admin.ModelAdmin):
-	pass
 
 # Register your models here.
-admin.site.register(Order, OrderAdmin)
-admin.site.register(OrderItem, OrderAdmin)

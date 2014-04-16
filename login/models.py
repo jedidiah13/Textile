53bde4f94eaa4c82613dff0e59e23658cea21358
devgr <devgr4@gmail.com>
2014-04-16 15:47:23 -0500

    Functioning
    Making progress on relations with user

--- login/models.py
+++ login/models.py
@@ -2,10 +2,15 @@ from django.contrib.auth.models import User
 from django.db import models
 from loginRouter import *
 
+class ohno(models.Model):
+    box = models.CharField(max_length=128)
 
+    def __unicode__(self): 
+        return self.user.username    
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
+    relation = models.OneToOneField(ohno, null=True)
     confirmation_code = models.CharField(max_length=128)
     reset_code = models.CharField(max_length=128)
     address_lineOne = models.CharField(max_length=128)

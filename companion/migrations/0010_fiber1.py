3a61122b2955b4a01bde3ab9c51b000e133bcd0f
Greg Goodman <sonic.logic@gmail.com>
2014-04-20 15:39:32 -0500

    Revert a2ea59b..a3ae061
    This rolls back to commit a2ea59b2fd8364c2922d4d10d9276944ea73470b.

---
+++ companion/migrations/0010_fiber1.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import DataMigration
+from django.db import models
+
+class Migration(DataMigration):
+
+    def forwards(self, orm):
+        "Write your forwards methods here."
+        # Note: Don't use "from appname.models import ModelName". 
+        # Use orm.ModelName to refer to models in this application,
+        # and orm['appname.ModelName'] for models in other applications.
+
+    def backwards(self, orm):
+        "Write your backwards methods here."
+
+    models = {
+        u'companion.catagories': {
+            'Meta': {'object_name': 'Catagories'},
+            'catagory': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'categoryId': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        },
+        u'companion.fabrics': {
+            'Meta': {'object_name': 'Fabrics'},
+            'fabContent': ('django.db.models.fields.CharField', [], {'max_length': '128', 'blank': 'True'}),
+            'fabDescription': ('django.db.models.fields.CharField', [], {'max_length': '8192'}),
+            'fabDye': ('django.db.models.fields.CharField', [], {'max_length': '128', 'blank': 'True'}),
+            'fabFinish': ('django.db.models.fields.CharField', [], {'max_length': '128', 'blank': 'True'}),
+            'fabImage': ('imagekit.models.fields.ProcessedImageField', [], {'max_length': '100', 'blank': 'True'}),
+            'fabImage_secondary': ('imagekit.models.fields.ProcessedImageField', [], {'max_length': '100', 'blank': 'True'}),
+            'fabName': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'fabTopic': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['companion.Topics']"}),
+            'fabVideo': ('embed_video.fields.EmbedVideoField', [], {'max_length': '200', 'blank': 'True'}),
+            'fabVideoURL': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'}),
+            'fabWeave': ('django.db.models.fields.CharField', [], {'max_length': '128', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'isPremium': ('django.db.models.fields.BooleanField', [], {'default': 'False'})
+        },
+        u'companion.topics': {
+            'Meta': {'object_name': 'Topics'},
+            'fabCatagory': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['companion.Catagories']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'topic': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'topicId': ('django.db.models.fields.CharField', [], {'max_length': '128'})
+        }
+    }
+
+    complete_apps = ['companion']
+    symmetrical = True

39ea8ac250131846795b1018275f6acdc1829c1b
pdf2e <pdf2e@mtmail.mtsu.edu>
2014-04-29 12:36:59 -0500

    Contact Form Now functional
    Contact Form Now functional

--- frontend/views.py
+++ frontend/views.py
@@ -1,6 +1,11 @@
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.core.context_processors import csrf
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
+from django.core.mail import send_mail
 from login.forms import RegistrationForm
 from login.forms import LoginForm
 from webstore.models import StoreItem
@@ -20,8 +25,24 @@ def about(request):
 	context = RequestContext(request)
 	return render_to_response('about.html', {'regform': RegistrationForm(),'loginform': LoginForm()},context)
 
+
+
+@csrf_exempt
 def contact(request):
-	context = RequestContext(request)
-	return render_to_response('contact.html', {'regform': RegistrationForm(),'loginform': LoginForm()},context)
+    context = RequestContext(request)
+    if(request.method == 'POST'):
+    	title = "Textile Fabric Consultants, Inc. Customer Contact"
+       	last = request.POST.get('last')
+        first = request.POST.get('first')
+        phone = request.POST.get('phone')
+        email = request.POST.get('email')
+        comments = request.POST.get('comments')
+        content = "Name: " + str(last) + "," + str(first)  + "Phone: " + str(phone)  + "Email: " + str(email)  + "Comments: " + str(comments)
+        
+        sendemail = [ "textilefc@textilefabric.com" ] 
+
+        send_mail(title, content, 'no-reply@gsick.com', sendemail, fail_silently=False)
+        return render_to_response('contact.html',{'success':True},context)
+    return render_to_response('contact.html',context)
 
 

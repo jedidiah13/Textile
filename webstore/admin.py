48cf6168a9d313b6ab25e7840516b13baf456a03
tothejp <tothejp@gmail.com>
2015-04-06 19:02:23 -0500

    I updated webstore admin.py. If It doesn't work, let me know

--- webstore/admin.py
+++ webstore/admin.py
@@ -1,16 +1,26 @@
 from django.contrib import admin
 from webstore.models import StoreCategory, StoreItem, Order, OrderItemCorrect
 
-
-
+class StoreItemAdmin(admin.ModelAdmin):
+		fieldsets = (
+		(None, { 'fields' : ('category' , 'itemName', 'itemNameid', 'description', 'price','quantity','picture')}),
+		('Advanced features', { 'classes' : ('collapse',), 'fields' : ('featured_picture', 'isFeatured', 'canCalcShipping', 'weightPerItem', 'numberPerBox', 'boxWidth', 'boxDepth', 'boxHeight', 'isFabric', 'isSmallItem','isSwatchKit', 'isFeltingKit')}),
+		)
+		list_display = ('category', 'itemName', 'picture')
+		list_display_links = ('category', 'itemName')
+		
+		
 class OrderAdmin(admin.ModelAdmin):
-	pass
+		fieldsets = (
+		(None, { 'fields' : ('purchaser' , 'orderDate', 'shippingCarrier','shippingType','shippedDate', 'deliveredDate')}),
+		('Advanced features', { 'classes' : ('collapse',), 'fields' : ('shippingCost', 'totalCost', 'shipToAddress', 'shipToState', 'shipToZipcode')}),
+		)
+		list_display = ('orderDate', 'purchaser', 'shippedDate')
+		list_display_links = ('orderDate', 'purchaser', 'shippedDate')
 
-
-class StoreAdmin(admin.ModelAdmin):
-	pass
 # Register your models here.
-admin.site.register(StoreItem, StoreAdmin)
-admin.site.register(StoreCategory, StoreAdmin)
+
+admin.site.register(StoreItem, StoreItemAdmin)
+admin.site.register(StoreCategory)
 admin.site.register(Order, OrderAdmin)
-admin.site.register(OrderItemCorrect, OrderAdmin)
+#admin.site.register(OrderItemCorrect)
